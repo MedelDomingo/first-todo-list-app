25c00d9e42dd8691fa324812e7c15a56fd287b19
medel.domingo <v-medel.domingo@lionbridge.com>
2025-10-07 14:00:23 +0800

    Adding short desc functionality and favicon

--- src/Components/TableHeader.tsx
+++ src/Components/TableHeader.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Button from "./Button";
 
+import favicon from "../assets/images/favicon.png";
+
 export default function TableHeader() {
   return (
     <div>
       <div className="table-header-wrapper flex items-center justify-between border-b-gray-200 pt-5 pb-10">
-        <div className="heading">
-          <h2 className="font-bold text-lg">My tasks</h2>
+        <div className="heading flex items-center">
+          <img src={favicon} alt="site__icon" />
+          <h2 className="font-bold text-lg ml-3">My tasks</h2>
         </div>
         <div className="options-buttons">
           <ul>
